bfebed2b6c953c2e8ce40790ff2309166fbb2a8d
Ripon Seikh <34369083+riponsk512@users.noreply.github.com>
2023-09-26 21:46:23 +0530

    Initial Commit

---
+++ SwiftdataTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftdataTest
+//
+//  Created by Ripon sk on 26/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftdataTest/SwiftdataTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftdataTestApp.swift
+//  SwiftdataTest
+//
+//  Created by Ripon sk on 26/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftdataTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
